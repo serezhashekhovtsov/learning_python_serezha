a40eb7784fe225d678b4ef49a54163a85d7f1597
shekhovtsovsergey <s.shekhovtsov@gmail.com>
2021-08-17 16:39:41 +0300

    Начал 3 главу

--- lessons/chapter 2/lesson 2.py
+++ lessons/chapter 2/lesson 2.py
@@ -16,3 +16,28 @@ print(f'\nПривет, {name1}, как у тебя дела?')
 print(f'\n{name1.title()}')
 print(f'{name1.upper()}')
 print(f'{name1.lower()}')
+
+#2.5
+print('\nAlbert Einstein once said, "A preson who never made\na mistake never tried anything new')
+
+#2.6
+famous = 'Albert Einstein'
+print(f'\n{famous} once said, "A preson who never made\na mistake never tried anything new')
+
+#2.7
+name_2 = '    serezha             '
+print(name_2.lstrip())
+print(name_2.rstrip())
+print(name_2.strip())
+
+#2.8
+print(f'\n{7+1}')
+print(9-1)
+print(16/2)
+print(4*2)
+
+#2.9
+fav_num = 42
+msg = (f"\nMy favorite number is {fav_num}.")
+print(msg)
+
---
+++ lessons/chapter 2/lesson 3.py
@@ -0,0 +1,16 @@
+#3.1
+names = ['artem', 'ayaz', 'nikita', 'zhenya', 'ksusha', 'egor', 'nastya']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+#3.2
+print(f'Hello, {names[0].title()}!')
+print(f'Hello, {names[2].title()}!')
+print(f'Hello, {names[2].title()}!')
+
+#3.3
+
+
