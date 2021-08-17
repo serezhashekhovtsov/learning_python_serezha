264b3b897803b7dc96a4b7f7108ac8ac6341edba
shekhovtsovsergey <s.shekhovtsov@gmail.com>
2021-08-17 14:53:52 +0300

    Удалил старые уроки, начал проходить заново

---
+++ lessons/chapter 2/lesson 2.py
@@ -0,0 +1,18 @@
+#2.1
+message = 'privet'
+print(message)
+
+#2.2
+message_1 = '\npaka'
+print(message_1)
+message_1 = 'kaka'
+print(message_1)
+
+#2.3
+name1 = 'serezha'
+print(f'\nПривет, {name1}, как у тебя дела?')
+
+#2.4
+print(f'\n{name1.title()}')
+print(f'{name1.upper()}')
+print(f'{name1.lower()}')
